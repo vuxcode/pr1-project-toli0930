72fb24d4efa6518fa1e5f973b5f048ff21bf8745
Tove Lindström <toli0930@skovde.com>
2023-11-28 23:33:27 +0100

    hitting ceiling or floor stops paddle

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const gameBoard = document.getElementById('game-board'); //canvas element
-const gameBoardWidth = gameBoard.width;
-const gameBoardHeight = gameBoard.height;
+const gameBoardWidth = gameBoard.width; //1350
+const gameBoardHeight = gameBoard.height; //585
 const context = gameBoard.getContext('2d'); //get access to the 2d interface things
+
 //because these values are used for two objects we can define them here
 //also because it didnt work when i tried to do this:
 //y: (gameBoard.height/2)-(leftPaddle.height/2)
 var paddleHeight = 80;
 var paddleWidth = 30;
-var leftIsPushing = false;
-var rightIsPushing = false;
+
+//bools for when the controlling keys are being pushed
+var upLeftIsPush = false;
+var downLeftIsPush = false;
+var upRightIsPush = false;
+var downRightIsPush = false;
 
 //objects are just containers with multiple variables of different types
-//we can define the paddles location and size in a single container
+//we can define the paddles location and size in a single container very convenient
 const leftPaddle = {
     x: 40, 
     y: (gameBoard.height/2)-(paddleHeight/2), //needs subtraction to be symmetrically in middle  
@@ -34,47 +39,75 @@ context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.he
 //and redraw it every frame
 //we need to manipulate the values of leftPaddle.y and rightPaddle.y in a function and run the function every frame
 
-addEventListener("keydown", movePaddle);
-addEventListener("keyup", stopMovingRight);
-addEventListener("keyup", stopMovingLeft)
-
-    function movePaddle(e){
-        //move left paddle
-            if(e.key == "w"){
-                leftIsPushing = true;
-                leftPaddle.y = leftPaddle.y - 5;
+addEventListener("keydown", function movePaddle(e){
+    //move left paddle
+    if(e.key == "w"){
+        upLeftIsPush = true;
+        if(upLeftIsPush == true){
+            leftPaddle.y = leftPaddle.y - 5;
             //parameters of clearRect() decide how much of the screen should be cleared
             //so if i divide the width by 2, it only clears half of it, i.e the left paddle
-                context.clearRect(0, 0, gameBoard.width / 2, gameBoard.height);
-                context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
-                console.log(leftPaddle.y);
-            } else if(e.key == "s"){
-                leftPaddle.y = leftPaddle.y + 5;
-                context.clearRect(0, 0, gameBoard.width / 2, gameBoard.height);
-                context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
-            }    
-        //move right paddle
-        if(rightIsPushing == true){
-            if(e.key == "ArrowUp"){
-                rightPaddle.y = rightPaddle.y - 5;
-                //instead of dividing how much of the screen i can decide the start coordinate. which is the half point of the screen
-                context.clearRect(gameBoardWidth/2, 0, gameBoard.width, gameBoard.height);
-                context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
-                console.log(e.key);
-            } else if(e.key == "ArrowDown"){
-                rightPaddle.y = rightPaddle.y + 5;
-                context.clearRect(gameBoardWidth/2, 0, gameBoard.width / 2, gameBoard.height);
-                context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
-            }    
+            //but wouldnt that clear the ball too, later? i guess well find out
+            context.clearRect(0, 0, gameBoard.width/2, gameBoard.height);
+            context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
+        }
+    } else if(e.key == "s"){
+        downLeftIsPush = true;
+        if(downLeftIsPush == true){
+            leftPaddle.y = leftPaddle.y + 5;
+            context.clearRect(0, 0, gameBoard.width/2, gameBoard.height);
+            context.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
         }
     }
+    //move right paddle
+    if(e.key == "ArrowUp"){
+        upRightIsPush = true;
+        if(upRightIsPush == true){
+            rightPaddle.y = rightPaddle.y - 5;
+            //instead of dividing how much of the screen i can decide the start coordinate. which is the half point of the screen
+            context.clearRect(gameBoardWidth/2, 0, gameBoard.width, gameBoard.height);
+            context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
+        }
+    } else if(e.key == "ArrowDown"){
+        downRightIsPush = true;
+        if(downRightIsPush == true){
+            rightPaddle.y = rightPaddle.y + 5;
+            context.clearRect(gameBoardWidth/2, 0, gameBoard.width / 2, gameBoard.height);
+            context.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
+        }
+    } 
+})
 
-    function stopMovingRight(e){
-        rightIsPushing = false;
+//paddles stop moving when key is released
+//i guess the reason this doesn't work is because it considers going to a different key to also be keyup
+//i understand the idea of adding the keys to an array instead, i just don't know how to write it syntax-wise
+addEventListener('keyup', function(e){
+    if(e.key == 'w' || e.key == 's'){
+        upLeftIsPush = false;
+        downLeftIsPush = false;
+    } else if(e.key == 'ArrowUp' || e.key == 'ArrowDown'){
+        upRightIsPush = false;
+        downRightIsPush = false;
     }
-    function stopMovingLeft(e){
-        leftIsPushing = false;
-    }
+})
 
-//because the two functions are almost identical i wonder if theres a way to combine them into one
+function gameLoop(){
+    //hitting ceiling or floor stops paddle
+    if(leftPaddle.y < 0){
+        leftPaddle.y = 0;
+    } else if(leftPaddle.y > 500){ //for some reason 500 works instead of the value of gameBoardHeight (585) ?????
+        leftPaddle.y = 500;
+        console.log("hit floor");
+    } else if(rightPaddle.y < 0){
+        rightPaddle.y = 0;
+    } else if(rightPaddle.y > 500){
+        rightPaddle.y = 500;
+        console.log("hit floor");
+    }
+}
+
+setInterval(gameLoop, 10);
+
+//to move the paddles simultaneously we can create an empty array and add the currently held keys to it
+//if array contains key move paddle
 
